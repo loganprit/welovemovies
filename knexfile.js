aa1525d044b0fa0031285d0cf180f92c9f7df403
Logan Pritchett <logan.pritchett01@gmail.com>
2024-05-10 21:20:20 -0500

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -21,7 +21,7 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: { DATABASE_URL, ssl: { rejectUnauthorized: false } },
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
