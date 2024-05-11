e96b58afe41b5b52e8625bc082d1a4c872a56077
Logan Pritchett <logan.pritchett01@gmail.com>
2024-05-10 21:07:48 -0500

    chore: Update server.js to use dynamic port configuration

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
-const port = process.env.PORT || 5001;
+const { PORT = 5001 } = process.env;
+
 const app = require("./app");
 const knex = require("./db/connection");
 
-const listener = () => console.log(`Listening on Port ${port}!`);
+const listener = () => console.log(`Listening on Port ${PORT}!`);
 
 knex.migrate
   .latest()
   .then((migrations) => {
     console.log("migrations", migrations);
-    app.listen(port, listener);
+    app.listen(PORT, listener);
   })
   .catch(console.error);
