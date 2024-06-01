be8573d13cd9a1496fee5c221caf469ad584d419
Logan Pritchett <logan.pritchett01@gmail.com>
2024-05-31 20:38:59 -0500

    Update DATABASE_URL in knexfile.js to use SSL connection

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,12 @@
 const path = require("path");
 require("dotenv").config();
 
-const {
-  DATABASE_URL = "postgres://avnadmin:AVNS_zMZ3eHqUGIy8A4j4ITS@welovemovies-welovemovies.e.aivencloud.com:19388/defaultdb?sslmode=require",
-} = process.env;
+const DATABASE_URL = process.env.DATABASE_URL;
 
 module.exports = {
   development: {
     client: "postgresql",
-    connection: DATABASE_URL + "?ssl=true",
+    connection: DATABASE_URL + "?sslmode=require",
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
