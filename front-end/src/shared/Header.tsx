41d3f4b03adfaba81213e7b9f7d9db946cef7bb5
Logan Pritchett <logan.pritchett01@gmail.com>
2024-12-06 01:30:28 -0600

    feat: update header image

--- front-end/src/shared/Header.tsx
+++ front-end/src/shared/Header.tsx
@@ -19,7 +19,7 @@ const Header: React.FC = () => {
     <header
       className="relative text-white border-b border-gray-800"
       style={{
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.7)), url(${headerImage})`,
+        backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.4)), url(${headerImage})`,
         backgroundPosition: "center",
         backgroundSize: "cover"
       }}
@@ -30,10 +30,10 @@ const Header: React.FC = () => {
           <ThemeToggle />
         </div>
         <div className="py-12">
-          <h1 className="text-5xl font-poppins-heading mb-4">
+          <h1 className="text-5xl font-poppins-heading mb-4 drop-shadow-[0_2px_4px_rgba(0,0,0,0.4)] tracking-tight">
             Find your next favorite movie!
           </h1>
-          <p className="text-2xl font-light">
+          <p className="text-2xl font-light drop-shadow-[0_1px_2px_rgba(0,0,0,0.3)]">
             <em className="font-medium">WeLoveMovies</em> is your source for finding reviews of movies in
             theaters near you.
           </p>
